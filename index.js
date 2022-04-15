9de3310f82183e04984be092e9ece32094e06cab
aha00a <aha00a@gmail.com>
2022-04-16 01:38:32 +0900

    implement parseInterpreter

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-module.exports = {
-    hello: name => `Hello, ${name ?? 'aha-mark'}.`
+
+const ahaMark = {
+    parseInterpreter: s => {
+        if (!s)
+            return [];
+
+        let array = s.split(/(?<!\\)(\[\[\[|]]])/).filter(_ => _);
+        let arrayIndexValue = array.reduce((a, v) => {
+            return {
+                i: a.i + v.length,
+                a: [...a.a, {i: a.i, v,}],
+            };
+        }, {i: 0, a: []}).a;
+
+        while(true) {
+            const indexClose = array.indexOf(']]]');
+            if(indexClose < 0)
+                return arrayIndexValue;
+
+            const indexOpen = array.lastIndexOf("[[[", indexClose);
+            if(indexOpen < 0)
+                return arrayIndexValue;
+
+            array = [
+                ...array.slice(0, indexOpen),
+                {v: array.slice(indexOpen + 1, indexClose)},
+                ...array.slice(indexClose + 1)
+            ];
+            arrayIndexValue = [
+                ...arrayIndexValue.slice(0, indexOpen),
+                {
+                    i: arrayIndexValue[indexOpen]?.i ?? 0,
+                    v: arrayIndexValue.slice(indexOpen + 1, indexClose),
+                },
+                ...arrayIndexValue.slice(indexClose + 1)
+            ];
+        }
+    },
 };
+
+module.exports = ahaMark;
--- index.test.js
+++ index.test.js
@@ -1,7 +1,48 @@
 const ahaMark = require('./index');
 
-test('ahaMark.hello', () => {
-    expect(ahaMark.hello()).toBe('Hello, aha-mark.');
-    expect(ahaMark.hello('AhaMark')).toBe('Hello, AhaMark.');
+
+test('ahaMark.parseInterpreter', () => {
+    expect(''.charAt(2)).toEqual('');
+
+    expect(ahaMark.parseInterpreter()).toEqual([]);
+    expect(ahaMark.parseInterpreter(null)).toEqual([]);
+    expect(ahaMark.parseInterpreter('')).toEqual([]);
+
+    expect(ahaMark.parseInterpreter('a')).toEqual([{i: 0, v: 'a',},]);
+    expect(ahaMark.parseInterpreter('aa')).toEqual([{i: 0, v: 'aa',},]);
+
+    expect(ahaMark.parseInterpreter('[[[]]]')).toEqual([{i: 0, v: []},]);
+    expect(ahaMark.parseInterpreter('[[[')).toEqual([{i: 0, v: '[[['},]);
+    expect(ahaMark.parseInterpreter(']]]')).toEqual([{i: 0, v: ']]]'},]);
+    expect(ahaMark.parseInterpreter('[[[a]]]')).toEqual([{i: 0, v: [{i: 3, v: 'a'}]},]);
+    expect(ahaMark.parseInterpreter('[[[aa]]]')).toEqual([{i: 0, v: [{i: 3, v: 'aa'}]},]);
+
+    expect(ahaMark.parseInterpreter('a[[[a]]]a')).toEqual([{i: 0, v: "a"}, {i: 1, v: [{i: 4, v: "a"}],}, {i: 8, v: "a"}]);
+    expect(ahaMark.parseInterpreter('ab[[[cd]]]ef')).toEqual([{i: 0, v: "ab"}, {i: 2, v: [{i: 5, v: "cd"}],}, {i: 10, v: "ef"}]);
+
+    expect(ahaMark.parseInterpreter('[[[]]][[[]]]')).toEqual([{i: 0, v: []}, {i: 6, v: []}]);
+    expect(ahaMark.parseInterpreter('[[[[[[]]]]]]')).toEqual([{i: 0, v: [{i: 3, v: []}]}]);
+    expect(ahaMark.parseInterpreter('[[[[[[]]][[[]]]]]]')).toEqual([{i: 0, v: [{i: 3, v: []}, {i: 9, v: []}]}]);
+
+    const abcdefghijklmn = ahaMark.parseInterpreter('ab[[[cd[[[ef]]]gh[[[ij]]]kl]]]mn');
+    expect(abcdefghijklmn).toEqual([
+        {i: 0, v: "ab"},
+        {
+            i: 2, v: [
+                {i: 5, v: "cd"},
+                {
+                    i: 7, v: [
+                        {i: 10, v: "ef"}
+                    ]
+                },
+                {i: 15, v: "gh"},
+                {
+                    i: 17, v: [{i: 20, v: "ij"}]
+                },
+                {i: 25, v: "kl"}
+            ]
+        },
+        {i: 30, v: "mn"}
+    ]);
 });
 
