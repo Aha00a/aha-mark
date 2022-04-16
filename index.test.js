e24f49e82e6723f33f11feeeac62af99ef8af1cd
aha00a <aha00a@gmail.com>
2022-04-17 03:07:31 +0900

    refactoring

--- index.test.js
+++ index.test.js
@@ -1,48 +1,69 @@
+const mapRecursive = require('map-recursive');
 const ahaMark = require('./index');
 
-
-test('ahaMark.parseInterpreter', () => {
-    expect(''.charAt(2)).toEqual('');
-
-    expect(ahaMark.parseInterpreter()).toEqual([]);
-    expect(ahaMark.parseInterpreter(null)).toEqual([]);
-    expect(ahaMark.parseInterpreter('')).toEqual([]);
-
-    expect(ahaMark.parseInterpreter('a')).toEqual([{i: 0, v: 'a',},]);
-    expect(ahaMark.parseInterpreter('aa')).toEqual([{i: 0, v: 'aa',},]);
-
-    expect(ahaMark.parseInterpreter('[[[]]]')).toEqual([{i: 0, v: []},]);
-    expect(ahaMark.parseInterpreter('[[[')).toEqual([{i: 0, v: '[[['},]);
-    expect(ahaMark.parseInterpreter(']]]')).toEqual([{i: 0, v: ']]]'},]);
-    expect(ahaMark.parseInterpreter('[[[a]]]')).toEqual([{i: 0, v: [{i: 3, v: 'a'}]},]);
-    expect(ahaMark.parseInterpreter('[[[aa]]]')).toEqual([{i: 0, v: [{i: 3, v: 'aa'}]},]);
-
-    expect(ahaMark.parseInterpreter('a[[[a]]]a')).toEqual([{i: 0, v: "a"}, {i: 1, v: [{i: 4, v: "a"}],}, {i: 8, v: "a"}]);
-    expect(ahaMark.parseInterpreter('ab[[[cd]]]ef')).toEqual([{i: 0, v: "ab"}, {i: 2, v: [{i: 5, v: "cd"}],}, {i: 10, v: "ef"}]);
-
-    expect(ahaMark.parseInterpreter('[[[]]][[[]]]')).toEqual([{i: 0, v: []}, {i: 6, v: []}]);
-    expect(ahaMark.parseInterpreter('[[[[[[]]]]]]')).toEqual([{i: 0, v: [{i: 3, v: []}]}]);
-    expect(ahaMark.parseInterpreter('[[[[[[]]][[[]]]]]]')).toEqual([{i: 0, v: [{i: 3, v: []}, {i: 9, v: []}]}]);
-
-    const abcdefghijklmn = ahaMark.parseInterpreter('ab[[[cd[[[ef]]]gh[[[ij]]]kl]]]mn');
-    expect(abcdefghijklmn).toEqual([
-        {i: 0, v: "ab"},
-        {
-            i: 2, v: [
-                {i: 5, v: "cd"},
-                {
-                    i: 7, v: [
-                        {i: 10, v: "ef"}
-                    ]
-                },
-                {i: 15, v: "gh"},
-                {
-                    i: 17, v: [{i: 20, v: "ij"}]
-                },
-                {i: 25, v: "kl"}
-            ]
-        },
-        {i: 30, v: "mn"}
-    ]);
+describe.each([
+    {
+        input: undefined,
+        expectParsed: []
+    },
+    {
+        input: null,
+        expectParsed: []
+    },
+    {
+        input: '',
+        expectParsed: []
+    },
+    {
+        input: 'a',
+        expectParsed: [{i: 0, v: 'a',},],
+    },
+    {
+        input: 'aa',
+        expectParsed: [{i: 0, v: 'aa',},],
+    },
+    {
+        input: '[[[]]]',
+        expectParsed: [{i: 0, v: []},],
+    },
+    {
+        input: '[[[',
+        expectParsed: [{i: 0, v: '[[['},],
+    },
+    {
+        input: ']]]',
+        expectParsed: [{i: 0, v: ']]]'},],
+    },
+    {
+        input: '[[[a]]]',
+        expectParsed: [{i: 0, v: [{i: 3, v: 'a'}]},],
+    },
+    {
+        input: '[[[aa]]]',
+        expectParsed: [{i: 0, v: [{i: 3, v: 'aa'}]},],
+    },
+    {
+        input: 'a[[[a]]]a',
+        expectParsed: [{i: 0, v: "a"}, {i: 1, v: [{i: 4, v: "a"}],}, {i: 8, v: "a"}],
+    },
+    {
+        input: 'ab[[[cd]]]ef',
+        expectParsed: [{i: 0, v: "ab"}, {i: 2, v: [{i: 5, v: "cd"}],}, {i: 10, v: "ef"}],
+    },
+    {
+        input: '[[[]]][[[]]]',
+        expectParsed: [{i: 0, v: []}, {i: 6, v: []}],
+    },
+    {
+        input: '[[[[[[]]]]]]',
+        expectParsed: [{i: 0, v: [{i: 3, v: []}]}],
+    },
+    {
+        input: '[[[[[[]]][[[]]]]]]',
+        expectParsed: [{i: 0, v: [{i: 3, v: []}, {i: 9, v: []}]}],
+    },
+])('ahaMark', ({input, expectParsed}) => {
+    test(`parseInterpreter\n${input}\n${expectParsed}`, () => {
+        expect(ahaMark.parseInterpreter(input)).toEqual(expectParsed);
+    });
 });
-
